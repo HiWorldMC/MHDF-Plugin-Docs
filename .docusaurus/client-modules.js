41c0b70d80e799105d9136156e3d1cb1c7cc7bb1
橙汁喵 <95158367+ChengZhiMeow@users.noreply.github.com>
2025-07-16 04:02:31 +0800

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\HiTech0926\\Projects\\MHDF-Plugin-Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\HiTech0926\\Projects\\MHDF-Plugin-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\HiTech0926\\Projects\\MHDF-Plugin-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\HiTech0926\\Projects\\MHDF-Plugin-Docs\\src\\css\\custom.css"),
-];
